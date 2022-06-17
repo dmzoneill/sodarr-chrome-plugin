a1e45cb78d3c9cbd9ab3d0e2ab003e606a5e27a2
David O Neill <dmz.oneill@gmail.com>
2022-06-17 20:28:58 +0100

    Make more resilient to REACT DOM changtes

--- chrome-plugin/contentscript.js
+++ chrome-plugin/contentscript.js
@@ -31,56 +31,71 @@ function xdg_open(path) {
 
 function open_episode(button) {
     button.click();
-    var modal = document.getElementsByClassName("Modal-modal-VB7eH");
+    var modal = document.querySelectorAll("div[class*='Modal-modal']");
     if(modal.length ==0) {
         console.log("Modal never appeared?");
         return;
     }
-    var columns = modal[0].getElementsByClassName("TableRowCell-cell-1CLnf");
+    var columns = modal[0].querySelectorAll("td[class*='TableRowCell-cell']");
+    console.log(columns);
     var video_path = columns[0].innerText;
     console.log(video_path);
-    document.getElementsByClassName("ModalContent-closeButton-3yvAL")[0].click();
+    document.querySelectorAll("button[class*='ModalContent-closeButton']")[0].click();
     xdg_open(video_path);
 }
 
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
 const update_page_episode_links = async function() {    
-    var series_episodes = document.getElementsByClassName("EpisodeTitleLink-link-2nsji");      
+    var series_episodes = [];      
     console.log(series_episodes);
+
+    removeElementsByClass("vlc_button");
     
     while(series_episodes.length == 0) {
         console.log("No episode links");
-        series_episodes = document.getElementsByClassName("EpisodeTitleLink-link-2nsji"); 
         await sleep(200);
+        series_episodes = document.querySelectorAll("button[class*='EpisodeTitleLink-link']");
     }
 
     console.log("Episodes links " + series_episodes.toString());
 
     for (let i = 0; i < series_episodes.length; i++) {
-        let img = document.createElement('img');
-        img.setAttribute("src", vlc_logo);
-        img.setAttribute("width", "12");
-        img.setAttribute("height", "12");
-        img.setAttribute("style", "margin-left:12px; cursor: pointer;");
-        img.addEventListener("click", function() { open_episode(series_episodes[i]) });
-        insertAfter(img, series_episodes[i]);
+        var title = series_episodes[i].parentNode.parentNode.nextSibling.nextSibling.querySelectorAll("span");
+        if(title[0].title.indexOf("Episode has not aired") == -1) {
+            let img = document.createElement('img');
+            img.setAttribute("src", vlc_logo);
+            img.setAttribute("width", "12");
+            img.setAttribute("height", "12");
+            img.setAttribute("style", "margin-left:12px; cursor: pointer;");
+            img.setAttribute("class", "vlc_button");
+            img.addEventListener("click", function() { open_episode(series_episodes[i]) });
+            insertAfter(img, series_episodes[i]);
+        }
     }
 }
 
 const update_movie = async function() {    
-    var movie_path = document.getElementsByClassName("MovieFileEditorRow-relativePath-VWAkB");      
+    var movie_path = [];      
     
     while(movie_path.length == 0) {
         console.log("No movie path");
-        movie_path = document.getElementsByClassName("MovieFileEditorRow-relativePath-VWAkB"); 
-        await sleep(500);
+        movie_path = document.querySelectorAll("td[class*='MovieFileEditorRow-relativePath']");
+        await sleep(300);
     }
 
     console.log("Movie link " + movie_path[0].innerText);
-    console.log(document.getElementsByClassName("MovieDetails-path-UGxp_"));
+    var path = document.querySelectorAll("span[class*='MovieDetails-path']");
+    console.log(path);
 
-    console.log(document.getElementsByClassName("MovieDetails-path-UGxp_")[0].innerText);
+    console.log(path[0].innerText);
     console.log(movie_path[0].innerText);
-    var movie_full_path = document.getElementsByClassName("MovieDetails-path-UGxp_")[0].innerText + "/" + movie_path[0].innerText;
+    var movie_full_path = path[0].innerText + "/" + movie_path[0].innerText;
 
     let img = document.createElement('img');
     img.setAttribute("src", vlc_logo);
@@ -88,6 +103,7 @@ const update_movie = async function() {
     img.setAttribute("height", "12");
     img.setAttribute("style", "margin-left:12px; cursor: pointer;");
     img.setAttribute("id", "film_link");
+    img.setAttribute("class", "vlc_button");
     movie_path[0].innerHTML += img.outerHTML;
     document.getElementById("film_link").addEventListener("click", function() { xdg_open(movie_full_path); });
 }
@@ -95,12 +111,12 @@ const update_movie = async function() {
 const subscribe_episode_expander = async function() {
     console.log("Add click listeners");
 
-    var expander_buttons = document.getElementsByClassName("SeriesDetailsSeason-expandButton-3fx28");
+    var expander_buttons = [];
 
     while(expander_buttons.length == 0) {
         console.log("No expander buttons");
-        expander_buttons = document.getElementsByClassName("SeriesDetailsSeason-expandButton-3fx28"); 
-        await sleep(200);
+        expander_buttons = document.querySelectorAll("button[class*='SeriesDetailsSeason-expandButton']");
+        await sleep(100);
     }
 
     console.log(expander_buttons);
@@ -117,6 +133,7 @@ const page_setup = async function() {
     if(window.location.href.includes(sonarrurl)) {
         console.log("subscribe click");
         subscribe_episode_expander();
+        update_page_episode_links();
     }
 
     if(window.location.href.includes(radarrurl)) {
@@ -126,6 +143,23 @@ const page_setup = async function() {
 }
 
 window.onload = function() {
+    /*
+    var mutationObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+          console.log(mutation);
+        });
+    });
+
+    mutationObserver.observe(document.documentElement, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+    });
+    */
+
     page_setup();
 };
 
