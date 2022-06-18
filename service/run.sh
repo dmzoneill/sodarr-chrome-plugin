461a73fac34fa22c3499a85dcbfaf590c02f28c4
David O Neill <dmz.oneill@gmail.com>
2022-06-18 15:31:44 +0100

    More resilient selectors

--- service/run.sh
+++ service/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 cd /home/dave/src/docker-media-center/config/sodarr-chrome-plugin/service
 
-nc -z 127.0.0.1 35000
+/usr/bin/nc -z 127.0.0.1 35000
 
 if [ $? -eq 1 ]; then
     echo "launching ... "
-    nohup uvicorn --host 0.0.0.0 --port 35000 xdg_open:app --reload &
+    /usr/bin/nohup /home/dave/.local/bin/uvicorn --host 0.0.0.0 --port 35000 xdg_open:app --reload &
 fi
