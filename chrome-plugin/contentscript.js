f4f8a82ab62bbeec4e8981383e02cf7c3e76369b
David O Neill <dmz.oneill@gmail.com>
2022-06-18 15:21:30 +0100

    More resilient selectors

--- chrome-plugin/contentscript.js
+++ chrome-plugin/contentscript.js
@@ -1,166 +1,120 @@
-var radarrurl = "http://192.168.0.30:7878/movie/";
-var sonarrurl = "http://192.168.0.30:8989/series/";
+var service_endpoint = "http://localhost:35000/open";
+var updating_dom_timer = null;
+var updating_dom_series_link_count = 0;
 var vlc_logo = chrome.runtime.getURL("vlc.png");
+var file_missing_indicators = ['Episode has not aired', 'Episode missing from disk'];
+
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-function insertAfter(newNode, existingNode) {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-}
-
-async function postData(url = '', data) {
+const postData = async function(url = '', data) {
     const response = await fetch(url, {
         method: 'POST',
         mode: 'no-cors',
         cache: 'no-cache',
-        credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         redirect: 'follow',
-        referrerPolicy: 'no-referrer',
         body: "video=" + data
     });
     return response.text();
 }
 
-function xdg_open(path) {
-    postData('http://localhost:35000/open', path).then(data => {
-        console.log(data);
-    });
+const createImageLink = function(id = null, size = "12px") {
+    let img = document.createElement('img');
+    img.setAttribute("src", vlc_logo);
+    img.setAttribute("width", size);
+    img.setAttribute("height", size);
+    img.setAttribute("style", "margin-left:" + size + "; cursor: pointer;");
+    img.setAttribute("class", "vlc_button");
+    if(id != null) img.setAttribute("id", id);
+    return img;
 }
 
-function open_episode(button) {
-    button.click();
-    var modal = document.querySelectorAll("div[class*='Modal-modal']");
-    if(modal.length ==0) {
-        console.log("Modal never appeared?");
-        return;
-    }
-    var columns = modal[0].querySelectorAll("td[class*='TableRowCell-cell']");
-    console.log(columns);
-    var video_path = columns[0].innerText;
-    console.log(video_path);
-    document.querySelectorAll("button[class*='ModalContent-closeButton']")[0].click();
-    xdg_open(video_path);
+const insertAfter = async function (newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
-function removeElementsByClass(className){
+const removeElementsByClass = async function(className){
     const elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
+    while(elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
     }
 }
 
-const update_page_episode_links = async function() {    
-    var series_episodes = [];      
-    console.log(series_episodes);
+const open_episode = async function(button) {
+    button.click();
+    var modal = document.querySelectorAll("div[class*='Modal-modal']");
 
-    removeElementsByClass("vlc_button");
+    if(modal.length == 0) return;
+
+    var columns = modal[0].querySelectorAll("td[class*='TableRowCell-cell']");
+    document.querySelectorAll("button[class*='ModalContent-closeButton']")[0].click();
+
+    postData(service_endpoint, columns[0].innerText).then(data => {
+        console.log(data);
+    });
+}
+
+const update_page_episode_links = async function() {     
+    var series_episodes = document.querySelectorAll("button[class*='EpisodeTitleLink-link']");
+
+    if(series_episodes.length == updating_dom_series_link_count) return;
     
-    while(series_episodes.length == 0) {
-        console.log("No episode links");
-        await sleep(200);
-        series_episodes = document.querySelectorAll("button[class*='EpisodeTitleLink-link']");
-    }
-
-    console.log("Episodes links " + series_episodes.toString());
+    updating_dom_series_link_count = series_episodes.length;
+    removeElementsByClass("vlc_button");
 
     for (let i = 0; i < series_episodes.length; i++) {
         var title = series_episodes[i].parentNode.parentNode.nextSibling.nextSibling.querySelectorAll("span");
-        if(title[0].title.indexOf("Episode has not aired") == -1) {
-            let img = document.createElement('img');
-            img.setAttribute("src", vlc_logo);
-            img.setAttribute("width", "12");
-            img.setAttribute("height", "12");
-            img.setAttribute("style", "margin-left:12px; cursor: pointer;");
-            img.setAttribute("class", "vlc_button");
+        if(file_missing_indicators.includes(title[0].title) == false) {
+            var img = createImageLink();
             img.addEventListener("click", function() { open_episode(series_episodes[i]) });
             insertAfter(img, series_episodes[i]);
         }
     }
 }
 
-const update_movie = async function() {    
-    var movie_path = [];      
+const update_movie = async function() {
+    if(document.getElementById("film_link") != null) return;
+
+    var movie_details = document.querySelectorAll("ul[class*='MovieDetails-tabList']");
+    if(movie_details.length == 0) return;
+
+    var tabs = movie_details[0].querySelectorAll("li");
+    var selected = movie_details[0].querySelectorAll('[aria-selected="true"]');
+    tabs[2].click();
+    var movie_path = document.querySelectorAll("td[class*='MovieFileEditorRow-relativePath']");
     
-    while(movie_path.length == 0) {
-        console.log("No movie path");
-        movie_path = document.querySelectorAll("td[class*='MovieFileEditorRow-relativePath']");
-        await sleep(300);
-    }
+    if(movie_path.length == 0) return;
 
-    console.log("Movie link " + movie_path[0].innerText);
     var path = document.querySelectorAll("span[class*='MovieDetails-path']");
-    console.log(path);
-
-    console.log(path[0].innerText);
-    console.log(movie_path[0].innerText);
-    var movie_full_path = path[0].innerText + "/" + movie_path[0].innerText;
-
-    let img = document.createElement('img');
-    img.setAttribute("src", vlc_logo);
-    img.setAttribute("width", "12");
-    img.setAttribute("height", "12");
-    img.setAttribute("style", "margin-left:12px; cursor: pointer;");
-    img.setAttribute("id", "film_link");
-    img.setAttribute("class", "vlc_button");
-    movie_path[0].innerHTML += img.outerHTML;
-    document.getElementById("film_link").addEventListener("click", function() { xdg_open(movie_full_path); });
-}
-
-const subscribe_episode_expander = async function() {
-    console.log("Add click listeners");
-
-    var expander_buttons = [];
-
-    while(expander_buttons.length == 0) {
-        console.log("No expander buttons");
-        expander_buttons = document.querySelectorAll("button[class*='SeriesDetailsSeason-expandButton']");
-        await sleep(100);
-    }
-
-    console.log(expander_buttons);
-
-    for (let i = 0; i < expander_buttons.length; i++) {
-        console.log("Add click listener");
-        expander_buttons[i].addEventListener("click", function() { update_page_episode_links() });
-    }
-}
-
-const page_setup = async function() {
-    console.log("Page loaded");
-
-    if(window.location.href.includes(sonarrurl)) {
-        console.log("subscribe click");
-        subscribe_episode_expander();
-        update_page_episode_links();
-    }
-
-    if(window.location.href.includes(radarrurl)) {
-        console.log("subscribe click");
-        update_movie();
-    }
-}
-
-window.onload = function() {
-    /*
-    var mutationObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-          console.log(mutation);
+    var target = document.querySelectorAll("span[class*='MovieDetails-links']");
+    target[0].innerHTML += createImageLink("film_link", "16px").outerHTML;
+    document.getElementById("film_link").addEventListener("click", function() { 
+        postData(service_endpoint, path[0].innerText + "/" + movie_path[0].innerText).then(data => {
+            console.log(data);
         });
+    });
+
+    selected[0].click();
+}
+
+const update_page = async function() {
+    update_page_episode_links();
+    update_movie();
+}
+
+const observePageChanges = async function() {
+    var mutationObserver = new MutationObserver(function () {
+        clearTimeout(updating_dom_timer);
+        updating_dom_timer = setTimeout(update_page, 150);
     });
 
     mutationObserver.observe(document.documentElement, {
         attributes: true,
-        characterData: true,
         childList: true,
         subtree: true,
-        attributeOldValue: true,
-        characterDataOldValue: true
     });
-    */
+}
 
-    page_setup();
-};
-
-console.log("Started");
+window.onload = observePageChanges;
