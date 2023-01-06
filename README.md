e549a1111f32ea6c7823d0c12e5b710a6f043e43
David O Neill <dmz.oneill@gmail.com>
2023-01-06 12:56:40 +0000

    Updates for systemd service

--- README.md
+++ README.md
@@ -1,12 +1,55 @@
 # sodarr-chrome-plugin
-Plugin for chrome that opens videos directly from sonarr and radarr, by adding a VLC like icon to the file listing.
+Plugin for chrome that opens videos directly from Sonarr and Radarr, by adding a VLC like icon to the file listing.
 
-add crontab and update service/run.sh.. 
-This will make sure the service is running.
+## install python requirements
+
+install into local user python includes
+```
+pip3 install fastapi uvicorn
+```
+
+## You have 2 Install options
+
+1. crontab (simpler)
+2. systemd
+
+### 1. Crontab (simpler)
+Add crontab and update service/run.sh.  This will make sure the service is running.
 
 ```
 */5 * * * * /home/.../sodarr-chrome-plugin/service/run.sh
 ```
+
+### 2. Systemd
+
+Create service file
+```
+vim ~/.config/systemd/user/sodarr.service
+```
+Content sodarr.service
+```
+[Unit]
+Description=Sodarr client service
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/home/dave/src/docker-media-center/config/sodarr-chrome-plugin/service/
+ExecStart=/home/dave/.local/bin/uvicorn --host 0.0.0.0 --port 35000 xdg_open:app --reload
+
+[Install]
+WantedBy=default.target
+```
+
+Enable the service
+```
+systemctl --user daemon-reload
+systemctl --user enable sodarr
+systemctl --user restart sodarr
+systemctl --user status sodarr
+```
+
+## Screenshots
 
 ![alt text](https://raw.githubusercontent.com/dmzoneill/sodarr-chrome-plugin/main/img/sonarr.png)
 
--- service/xdg_open.py
+++ service/xdg_open.py
@@ -1,37 +1,72 @@
 from fastapi import FastAPI, Form
-import subprocess, os
+import subprocess
+import os
 from pprint import pprint
 import os
 import pwd
+import json
+
+try:
+    import systemd.daemon
+except:
+    pass
+
 
 def get_username():
     return pwd.getpwuid(os.getuid())[0]
 
+
+def check_valid_video(infile):
+    try:
+        if os.path.isdir(infile):
+            return False
+        elif os.path.isfile(infile):
+            lower = infile.lower()
+            extensions = ['.mp3', '.flac', '.avi',
+                          '.mkv', '.avi', '.mp4', '.mpg']
+            for ext in extensions:
+                if lower.endswith(ext):
+                    return [True, lower, ext]
+            return [False, lower, None]
+    except Exception as e:
+        return [False, False, None, str(e)]
+
+
 app = FastAPI()
 
 env = {
-    "DBUS_SESSION_BUS_ADDRESS":"unix:path=/run/user/" + str(os.getuid()) + "/bus",
-    "DESKTOP_SESSION":"gnome",
-    "DISPLAY":":1",
-    "GDM_LANG":"en_IE.UTF-8",
-    "HOME":"/home/" + get_username(),
-    "LANG":"en_IE.UTF-8",
-    "LANGUAGE":"en_IE:en",
-    "SHELL":"/bin/bash",
-    "SHLVL":"1",
-    "TERM":"xterm-256color",
-    "VTE_VERSION":"7000",
-    "WINDOWPATH":"2",
-    "XAUTHORITY":"/run/user/" + str(os.getuid()) + "/gdm/Xauthority",
-    "XDG_CURRENT_DESKTOP":"GNOME",
-    "XDG_DATA_DIRS":"/usr/share/gnome:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop",
-    "XDG_MENU_PREFIX":"gnome-",
-    "XDG_RUNTIME_DIR":"/run/user/" + str(os.getuid()),
-    "XDG_SESSION_CLASS":"user",
-    "XDG_SESSION_DESKTOP":"gnome",
-    "XDG_SESSION_TYPE":"x11",
-    "XMODIFIERS":"@im=ibus"
+    "DBUS_SESSION_BUS_ADDRESS": "unix:path=/run/user/" + str(os.getuid()) + "/bus",
+    "DESKTOP_SESSION": "gnome",
+    "DISPLAY": ":1",
+    "GDM_LANG": "en_IE.UTF-8",
+    "HOME": "/home/" + get_username(),
+    "LANG": "en_IE.UTF-8",
+    "LANGUAGE": "en_IE:en",
+    "SHELL": "/bin/bash",
+    "SHLVL": "1",
+    "TERM": "xterm-256color",
+    "VTE_VERSION": "7000",
+    "WINDOWPATH": "2",
+    "XAUTHORITY": "/run/user/" + str(os.getuid()) + "/gdm/Xauthority",
+    "XDG_CURRENT_DESKTOP": "GNOME",
+    "XDG_DATA_DIRS": "/usr/share/gnome:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop",
+    "XDG_MENU_PREFIX": "gnome-",
+    "XDG_RUNTIME_DIR": "/run/user/" + str(os.getuid()),
+    "XDG_SESSION_CLASS": "user",
+    "XDG_SESSION_DESKTOP": "gnome",
+    "XDG_SESSION_TYPE": "x11",
+    "XMODIFIERS": "@im=ibus"
 }
+
+
+@app.on_event("startup")
+async def startup_event():
+    print('Startup complete')
+    try:
+        systemd.daemon.notify('READY=1')
+    except:
+        pass
+
 
 @app.get("/")
 def do_nothing():
@@ -40,7 +75,9 @@ def do_nothing():
 
 @app.post("/open")
 async def play_video(video: str = Form(...)):
-    cmd = "/usr/bin/xdg-open \"" + video + "\""
-    print(cmd)
-    pprint(subprocess.call(cmd, shell=True, env=env, universal_newlines=True))
-    return video
+    path_check = check_valid_video(video)
+    if path_check[0]:
+        cmd = "/usr/bin/xdg-open \"" + video + "\""
+        print(cmd)
+        pprint(subprocess.call(cmd, shell=True, env=env, universal_newlines=True))
+    return json.dumps(path_check)
